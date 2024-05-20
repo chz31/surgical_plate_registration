393d97f92d8ef82b590d2155806a0e5c0db892eb
Chi Zhang <80793828+chz31@users.noreply.github.com>
2024-05-20 08:05:48 -0500

    Add files via upload

--- intersect_plate_orbit.py
+++ intersect_plate_orbit.py
@@ -1,11 +1,9 @@
-masterVolumeNode = slicer.util.getNode('5: DummySeriesDesc!')
+masterVolumeNode = slicer.util.getNode('5 DummySeriesDesc!')
 
 #Get segments
 segmentationNode = slicer.util.getNode('plate_heatmap-segmentation')
-segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(masterVolumeNode)
+# segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(masterVolumeNode)
 segmentationNode.CreateDefaultDisplayNodes() 
-boneSeg = segmentationNode.GetSegmentation().GetSegment('bone_copy')
-plateSeg = segmentationNode.GetSegmentation().GetSegment('plate_copy')
 
 #Create segment editor to get access to effects
 segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
@@ -13,14 +11,55 @@ segmentEditorWidget.setMRMLScene(slicer.mrmlScene)
 segmentEditorNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentEditorNode")
 segmentEditorWidget.setMRMLSegmentEditorNode(segmentEditorNode)
 segmentEditorWidget.setSegmentationNode(segmentationNode)
-segmentEditorWidget.setMasterVolumeNode(masterVolumeNode)
+# segmentEditorWidget.setMasterVolumeNode(masterVolumeNode)
 
 #Logic operator
 segmentEditorWidget.setActiveEffectByName("Logical operators")
 effect = segmentEditorWidget.activeEffect()
 effect.setParameter("Operation","INTERSECT")
+
 boneSegID = segmentationNode.GetSegmentation().GetSegmentIdBySegmentName('bone_no_fx')
 plateSegID = segmentationNode.GetSegmentation().GetSegmentIdBySegmentName('plate_copy')
+boneSeg = segmentationNode.GetSegmentation().GetSegment(boneSegID)
+plateSeg = segmentationNode.GetSegmentation().GetSegment(plateSegID)
+
+
 segmentEditorNode.SetSelectedSegmentID(plateSegID)
 effect.setParameter("ModifierSegmentID", boneSegID)
 effect.self().onApply()
+
+#Grow margin
+# print(segmentEditorWidget.availableEffectNames())
+segmentEditorWidget.setActiveEffectByName("Margin")
+effect = segmentEditorWidget.activeEffect()
+effect.setParameter("MarginSizeMm", 0.25)
+segmentEditorNode.SetSelectedSegmentID(plateSegID)
+effect.self().onApply()
+
+#Remove bone segment
+segmentationNode.GetSegmentation().RemoveSegment(boneSegID)
+
+
+#Create a new segment
+# segNode = slicer.vtkMRMLSegmentationNode()
+# slicer.mrmlScene.AddNode(segNode)
+# segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(masterVolumeNode)
+# segmentation = segNode.GetSegmentation()
+# segment1 = segmentaion.AddSegment(plateSeg)
+
+
+
+#Paint plate model by the plate_intersect segment
+# InputModelNode = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLModelNode")
+# OutputModelNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLModelNode", "Skull_Thickness")
+InputModelNode = slicer.util.getNode('Preformed Orbital small right  04_503_811')
+OutputModelNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLModelNode", "plate_painted")
+
+
+parameters = {}
+parameters['InputVolume'] = segmentationNode.GetID()
+parameters['InputModel'] = InputModelNode.GetID()
+parameters['OutputModel'] = OutputModelNode.GetID()
+probe = slicer.modules.probevolumewithmodel
+slicer.cli.run(probe, None, parameters, wait_for_completion=True)
+
